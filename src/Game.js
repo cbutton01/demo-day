8797011bcd2d6d05826a23026ff07b0668ee6c1a
cb <csbutton01@hotmail.com>
2018-08-31 13:53:02 -0700

    add a test image to play around with the sprite component

--- src/Game.js
+++ src/Game.js
@@ -1,16 +1,17 @@
 import React, { Component } from 'react';
-import { Loop, Stage } from 'react-game-kit';
+import { Loop, Stage, Sprite } from 'react-game-kit';
 import World from 'matter-js';
+import testimage from './assets/sprites/test-image.png';
 
 class Game extends Component {
     render() {
       return (
         <Loop>
           <Stage width={1024} height={576}>
-            
+            <Sprite offset={[0]} repeat={false} src={testimage}></Sprite>
           </Stage>
         </Loop>
-      )
+      );
     }
   }
 
