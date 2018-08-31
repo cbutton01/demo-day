a4f57ad76c723dd3227f265ab4a39317b81478dd
cb <csbutton01@hotmail.com>
2018-08-31 14:16:02 -0700

    change scale of sprite component to 1, now shows proper dimensions, but still no image being rendered

--- src/Game.js
+++ src/Game.js
@@ -1,14 +1,13 @@
 import React, { Component } from 'react';
 import { Loop, Stage, Sprite } from 'react-game-kit';
-import World from 'matter-js';
 import testimage from './assets/sprites/test-image.png';
 
 class Game extends Component {
     render() {
       return (
         <Loop>
-          <Stage width={1024} height={576}>
-            <Sprite offset={[0]} repeat={false} src={testimage}></Sprite>
+          <Stage dimensions={[0]} width={1024} height={576}>
+            <Sprite scale={1} offset={[0]} repeat={false} src="./assets/sprites/test-image.png"></Sprite>
           </Stage>
         </Loop>
       );
